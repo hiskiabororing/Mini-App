b78447a4a00ff48d4af143b7a05f074ead871f1e
hiskiabororing <69854351+hiskiabororing@users.noreply.github.com>
2021-10-11 16:34:19 +0800

    Add files via upload

---
+++ ADD STUDENT.js
@@ -0,0 +1,7 @@
+// Add Student Combobox hotfix
+$("#sel1").change(function () {
+    var id = $(this).val();
+	var options = $(this).data('options').filter('[class=' + id + ']' || '[class="0"]');
+	$('#sel2').html(options);
+}); 
+
---
+++ DefArray.js
@@ -0,0 +1,27 @@
+//default array
+var studentsData = [
+	{
+		id: 1,
+		NIM: 123545621558,
+		fullName: 'Hiskia Bororing',
+		gender: 'Male',
+		fakultas: 'FIK',
+		study: 'Sistem Informasi',
+	},
+	{
+		id: 2,
+		NIM: 10305454451,
+		fullName: 'Lalisa Blackpink',
+		gender: 'Female',
+		fakultas: 'FEB',
+		study: 'Manajemen',
+	},
+	{
+		id: 3,
+		NIM: 158781454502,
+		fullName: 'Anya Geraldine',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Informatika',
+	}
+]
